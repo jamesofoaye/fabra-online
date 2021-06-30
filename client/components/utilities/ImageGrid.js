a9cd21285624550a231a9ae35b73d4be17b18f04
Wematu James <55028697+wematujames@users.noreply.github.com>
2021-06-30 16:27:56 +0000

    Update

--- client/components/utilities/ImageGrid.js
+++ client/components/utilities/ImageGrid.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import useFirestore from '../../hooks/useFirestore';
-import { motion } from 'framer-motion';
+import React from "react";
+import useFirestore from "../../hooks/useFirestore";
+import { motion } from "framer-motion";
 
-const ImageGrid = ({ setSelectedImg }) => {
-  const { docs } = useFirestore('images');
-
+const ImageGrid = ({ setSelectedImg, adBucket }) => {
+  const { docs } = useFirestore(adBucket);
   return (
     <div className="img-grid">
-      {docs && docs.map(doc => (
-        <motion.div className="img-wrap" key={doc.id} 
-          layout
-          whileHover={{ opacity: 1 }}s
-          onClick={() => setSelectedImg(doc.url)}
-        >
-          <motion.img src={doc.url} alt="uploaded pic"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1 }}
-          />
-        </motion.div>
-      ))}
+      {docs &&
+        docs.map((doc) => (
+          <motion.div
+            className="img-wrap"
+            key={doc.url}
+            layout
+            whileHover={{ opacity: 1 }}
+            s
+          >
+            <motion.img
+              src={doc.url}
+              alt="uploaded pic"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+            {/* <button className="delete">
+              <div className="del-icon">X</div>
+            </button> */}
+          </motion.div>
+        ))}
     </div>
-  )
-}
-
+  );
+};
 export default ImageGrid;
