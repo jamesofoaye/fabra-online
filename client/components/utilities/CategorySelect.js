1ad300f10d5a07e25474ea7983a36c6b4caf1cf1
Wematu James <55028697+wematujames@users.noreply.github.com>
2021-06-30 17:00:31 +0000

    Update

--- client/components/utilities/CategorySelect.js
+++ client/components/utilities/CategorySelect.js
@@ -1,25 +1,40 @@
-import { FormLabel, Select, FormControl, Input, FormHelperText, Button, Center, Heading, Text, NumberDecrementStepper, NumberIncrementStepper, NumberInputStepper, NumberInputField, NumberInput, Textarea, Stack } from "@chakra-ui/react";
+import {
+  FormLabel,
+  Select,
+  FormControl,
+  Input,
+  FormHelperText,
+  Button,
+  Center,
+  Heading,
+  Text,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInputStepper,
+  NumberInputField,
+  NumberInput,
+  Textarea,
+  Stack
+} from "@chakra-ui/react";
 import { graphql } from "react-apollo";
 import { getCategories } from "../../queries/queries";
 
 const CategorySelect = (props) => {
-    //get list of Categories
-    const showCategories = () => {
-        if (props.data.loading) {
-            return (<option disabled> Loading...</option>)
-        } else {
-            return (
-                props.data.categories.map(category => {
-                    return (
-                        <option value={category.id}> {category.name}</option>
-                    )
-                })
-            )
-        }
+  //get list of Categories
+  const showCategories = () => {
+    if (props.data.loading) {
+      return <option disabled> Loading...</option>;
+    } else {
+      return props.data.categories.map((category) => {
+        return (
+          <option key={category.id} value={category.id}>
+            {" "}
+            {category.name}
+          </option>
+        );
+      });
     }
-    return (<>
-            {showCategories()}
-    </>
-    )
-}
+  };
+  return <>{showCategories()}</>;
+};
 export default graphql(getCategories)(CategorySelect);
