04ffac8ddaa0496b24263fb69c4306b6829eed8e
Wematu James <55028697+wematujames@users.noreply.github.com>
2021-06-30 16:29:23 +0000

    Update

--- client/pages/post-ad.js
+++ client/pages/post-ad.js
@@ -1,12 +1,24 @@
 import {
-  FormLabel, Select, FormControl, Input, FormHelperText,
-  Button, Center, Heading, NumberDecrementStepper,
-  NumberIncrementStepper, NumberInputStepper,
-  NumberInputField, NumberInput, Textarea, Stack
+  FormLabel,
+  Select,
+  FormControl,
+  Input,
+  FormHelperText,
+  Button,
+  Center,
+  Heading,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInputStepper,
+  NumberInputField,
+  NumberInput,
+  Textarea,
+  Stack
 } from "@chakra-ui/react";
 import Head from "next/head";
 
 //written for backend
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import CategorySelect from "../components/utilities/CategorySelect";
 import UploadForm from "../components/utilities/UploadForm";
@@ -15,38 +27,43 @@ import ImageGrid from "../components/utilities/ImageGrid";
 import { graphql, Mutation } from "react-apollo";
 import { newAdMutation } from "../queries/queries";
 
+const adBucket = uuidv4();
+
 const Post_An_Ad = (props) => {
- const [adInfo, setadInfo] = useState({
-   title: "",
-   description: "",
-   price: null,
-   location: "",
-   category: "",
-   gallery: []
- });
+  const [adInfo, setadInfo] = useState({
+    title: "",
+    description: "",
+    price: null,
+    location: "",
+    category: "",
+    gallery: []
+  });
 
-const handleInputChange = (e) => {
-  const name = e.target.name;
-  const value = e.target.value
-  setadInfo(preVal => {
-    return {...preVal, [name]: value}
-  })
-}
+  const handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setadInfo((preVal) => {
+      return { ...preVal, [name]: value };
+    });
+  };
 
-const submitForm = (e) => {
-  e.preventDefault()
-  props.newAdMutation({
-    variables:{
-      title: adInfo.title,
-      description: adInfo.description,
-      price: adInfo.price,
-      location: adInfo.location,
-      categoryId: adInfo.category,
-      gallery: adInfo.gallery
-    }
-  }
-  )
-}
+  const submitForm = (e) => {
+    e.preventDefault();
+    props.newAdMutation({
+      variables: {
+        title: adInfo.title,
+        description: adInfo.description,
+        price: adInfo.price,
+        location: adInfo.location,
+        categoryId: adInfo.category,
+        gallery: adInfo.gallery
+      }
+    });
+  };
+
+  const adImageUrl = (url) => {
+    adInfo.gallery.push(url);
+  };
   return (
     <>
       <Head>
@@ -55,92 +72,100 @@ const submitForm = (e) => {
       </Head>
 
       <Center>
-        <Heading color="brand.200"
-          py={2}>
+        <Heading color="brand.200" py={2}>
           Post Ad
         </Heading>
       </Center>
-<form action="/post-ad" method="POST">
-      <Stack px={{ base: 8, md: "30%" }}>
-        <FormControl>
-          <FormLabel>Title</FormLabel>
-          <Input name="title" value={adInfo.title}
-          onChange={handleInputChange}
-            autoFocus
-            aria-label="Title"
-            name="title"
-            required
-            placeholder="Please enter the name of your ad."
-          />
-        </FormControl>
-
-        <FormControl>
-          <FormLabel>Description</FormLabel>
-          <Textarea name="description" value={adInfo.description}
-          onChange={handleInputChange}
-            aria-label="Description"
-            name="description"
-            required
-            placeholder="Please enter description."
-          />
-        </FormControl>
-
-        <FormControl>
-          <FormLabel>Price</FormLabel>
-          <NumberInput>
-            <NumberInputField name="price" value={adInfo.price}
-            onChange={handleInputChange}
+      <form action="/post-ad" method="POST">
+        <Stack px={{ base: 8, md: "30%" }}>
+          <FormControl>
+            <FormLabel>Title</FormLabel>
+            <Input
+              name="title"
+              value={adInfo.title}
+              onChange={handleInputChange}
+              autoFocus
+              aria-label="Title"
+              name="title"
               required
-              aria-label="Price"
-              name="price"
-              placeholder="Please enter price."
+              placeholder="Please enter the name of your ad."
             />
-          </NumberInput>
-        </FormControl>
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Location</FormLabel>
-          <Input name="location" value={adInfo.location}
-          onChange={handleInputChange}
-            aria-label="Location"
-            name="location"
-            required
-            placeholder="Please enter your location."
-          />
-        </FormControl>
+          <FormControl>
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              name="description"
+              value={adInfo.description}
+              onChange={handleInputChange}
+              aria-label="Description"
+              required
+              placeholder="Please enter description."
+            />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Category</FormLabel>
-          <Select placeholder="Select Category" 
-          required onChange={handleInputChange} 
-          value={adInfo.category} 
-          name="category">
-            <CategorySelect/>
-          </Select>
-          <FormHelperText>
-            Which category does your product or service falls in?
-          </FormHelperText>
-        </FormControl>
-        {/* Photo Upload Form */}
-        <div>
-        <UploadForm/>
-        <ImageGrid/>
-        </div>
-        {/* Photo Upload Form */}
-        <Button
-          onClick={submitForm}
-          type="submit" mt={10}
-          bgGradient="linear(to-r,brand.200,brand.100)"
-          _hover={{ bg: "brand.100" }}
-          color="white"
-          variant="solid"
-        >
-          Post Ad
-        </Button>
-      </Stack>
+          <FormControl>
+            <FormLabel>Price</FormLabel>
+            <NumberInput>
+              <NumberInputField
+                name="price"
+                value={adInfo.price}
+                onChange={handleInputChange}
+                required
+                aria-label="Price"
+                placeholder="Please enter price."
+              />
+            </NumberInput>
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Location</FormLabel>
+            <Input
+              name="location"
+              value={adInfo.location}
+              onChange={handleInputChange}
+              aria-label="Location"
+              required
+              placeholder="Please enter your location."
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Category</FormLabel>
+            <Select
+              placeholder="Select Category"
+              required
+              onChange={handleInputChange}
+              value={adInfo.category}
+              name="category"
+            >
+              <CategorySelect />
+            </Select>
+            <FormHelperText>
+              Which category does your product or service falls in?
+            </FormHelperText>
+          </FormControl>
+          {/* Photo Upload Form */}
+          <div>
+            <UploadForm adImageUrl={adImageUrl} adBucket={adBucket} />
+            <div>
+              <ImageGrid imagesToShow={adInfo.gallery} adBucket={adBucket} />
+            </div>
+          </div>
+          {/* Photo Upload Form */}
+          <Button
+            onClick={submitForm}
+            type="submit"
+            mt={51}
+            bgGradient="linear(to-r,brand.200,brand.100)"
+            _hover={{ bg: "brand.100" }}
+            color="white"
+            variant="solid"
+          >
+            Post Ad
+          </Button>
+        </Stack>
       </form>
-      {console.log(adInfo)}
     </>
   );
-}
-export default graphql(newAdMutation, {name: "newAdMutation"})(Post_An_Ad);
+};
+export default graphql(newAdMutation, { name: "newAdMutation" })(Post_An_Ad);
